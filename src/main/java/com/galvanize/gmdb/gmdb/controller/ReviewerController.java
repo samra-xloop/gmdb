0b176d6f4f9549cd172060f7d90516bd8080a444
samra-xloop <samra.almas@xloopdigital.com>
2023-01-19 06:38:48 +0500

    Story 3 complete

--- src/main/java/com/galvanize/gmdb/gmdb/controller/ReviewerController.java
+++ src/main/java/com/galvanize/gmdb/gmdb/controller/ReviewerController.java
@@ -1,10 +1,39 @@
 package com.galvanize.gmdb.gmdb.controller;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.galvanize.gmdb.gmdb.model.Reviewer;
+import com.galvanize.gmdb.gmdb.repository.IReviewersRepository;
 
 @RestController
 @RequestMapping("/reviewers")
 public class ReviewerController {
+    @Autowired
+    public IReviewersRepository repository;
+
+
+    @GetMapping("")
+    public List<Reviewer> getAllReviewers(){
+        return this.repository.findAll();
+    }
+    @GetMapping("/{id}") 
+    public Reviewer getById(@PathVariable Long id){
+        return repository.findById(id).orElse(null);
+    }
+
+    @PostMapping("")  
+    public void createMovies(@RequestBody Reviewer reviewer) {
+        reviewer.setNumber_of_reviews(0);
+        reviewer.setJoining_date(Date.valueOf(LocalDate.now()));
+        this.repository.save(reviewer);
+    }
+
     
 }
--- src/main/java/com/galvanize/gmdb/gmdb/model/Reviewer.java
+++ src/main/java/com/galvanize/gmdb/gmdb/model/Reviewer.java
@@ -2,21 +2,39 @@ package com.galvanize.gmdb.gmdb.model;
 
 import java.sql.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.*;
 
 @Entity
 @Table(name="reviwers")
 public class Reviewer {
+
+    //Fields
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String username;
-    @Column(columnDefinition = "date")
+    @Column(columnDefinition = "date") 
+    @JsonFormat(pattern = "YYYY-MM-DD")
     private Date joining_date;
-
     private Integer number_of_reviews;
 
+
+    // Constructors
+    protected Reviewer(){}
+    public Reviewer(Long id,String username, Date joining_date, Integer num_of_reviews){
+        this.id=id;
+        this.username=username;
+        this.joining_date=joining_date;
+        this.number_of_reviews=num_of_reviews;
+    }
+    public Reviewer(String username){
+        this.username = username;
+    }
+
+
+    //GETTERS & SETTERS
     public String getUsername() {
         return username;
     }
